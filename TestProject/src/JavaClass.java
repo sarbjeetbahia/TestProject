c70187b4a22aa19a91d6f37d894638b3b5aaf929
sarbjeetbahia <sarbjeetbahia@sarbjeets-mbp-2.t-mobile.com>
2020-07-22 14:49:54 -0700

    pushing JavaClass

---
+++ TestProject/src/JavaClass.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaClass {
+	
+	public static void main(String[] args) {
+		//System.out.print("Hello");
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/sarbjeetbahia/Desktop/Selenium/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.yahoo.com/");
+	}
+
+}
